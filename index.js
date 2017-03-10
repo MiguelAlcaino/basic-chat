c4a012c5d9ca5f21e8247e94bcb34e44e345630b
Miguel Alcaino <miguel@buzzradar.com>
2017-03-10 15:41:39 +0000

    Added support for usernames with the command /nickname

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-
+var numberOfActiveConnections = 0;
 app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
 });
 
 io.on('connection', function(socket){
+    numberOfActiveConnections++;
     console.log("A user connected");
+    io.emit('username', 'User'+numberOfActiveConnections);
     socket.on('disconnect', function(){
-        console.log('User disconnected');
+        console.log('User disconnected'); 
+        numberOfActiveConnections--;
     });
 
-    socket.on('chat message', function(msg){
-        console.log('Message: '+msg);
-        io.emit('chat message',msg);
+    socket.on('chat message', function(username,msg){
+        io.emit('chat message', username, msg);
     });
 });
 
